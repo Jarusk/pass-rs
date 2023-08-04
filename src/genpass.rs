1f31a80056e7dedea6509fcf280a042fa85d3ff7
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2023-08-04 16:59:24 -0400

    Added initial test cases

--- src/genpass.rs
+++ src/genpass.rs
@@ -5,6 +5,9 @@ use rand::{Rng, RngCore, SeedableRng};
 use crate::constants;
 use crate::parseargs::ConfigArgs;
 
+#[cfg(test)]
+const MAX_ALPHABET_LENGTH: usize = 86;
+
 pub struct Alphabet {
     chars: Vec<char>,
     range: Uniform<usize>,
@@ -15,7 +18,7 @@ impl Alphabet {
     pub fn new(config: &ConfigArgs) -> Alphabet {
         // Enough space to hold all the alphanumeric characters (upper and lower case)
         // as well as the 14 special characters defined in constants.rs
-        let mut chars: Vec<char> = Vec::with_capacity(76);
+        let mut chars: Vec<char> = Vec::with_capacity(86);
 
         if config.enable_special {
             chars.append(&mut constants::SPECIAL_CHARS.to_vec());
@@ -53,5 +56,109 @@ impl Alphabet {
 
     pub fn get_char(&mut self) -> char {
         self.chars[self.rng.sample(self.range)]
+    }
+
+    #[cfg(test)]
+    pub fn set_seed(&mut self, seed: [u8; 32]) {
+        self.rng = StdRng::from_seed(seed);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{constants, genpass::MAX_ALPHABET_LENGTH, parseargs::ConfigArgs};
+
+    use super::*;
+
+    #[test]
+    fn it_makes_digit_only_alphabet() {
+        let mut cfg = ConfigArgs::new();
+        cfg.enable_digit = true;
+        cfg.enable_lower = false;
+        cfg.enable_upper = false;
+        cfg.enable_special = false;
+
+        let alpha = Alphabet::new(&cfg);
+
+        for elem in alpha.chars.iter() {
+            assert_eq!(elem.is_ascii_digit(), true);
+        }
+    }
+
+    #[test]
+    fn it_makes_lowercase_only_alphabet() {
+        let mut cfg = ConfigArgs::new();
+        cfg.enable_digit = false;
+        cfg.enable_lower = true;
+        cfg.enable_upper = false;
+        cfg.enable_special = false;
+
+        let alpha = Alphabet::new(&cfg);
+
+        for elem in alpha.chars.iter() {
+            assert_eq!(elem.is_ascii_lowercase(), true);
+        }
+    }
+
+    #[test]
+    fn it_makes_uppercase_only_alphabet() {
+        let mut cfg = ConfigArgs::new();
+        cfg.enable_digit = false;
+        cfg.enable_lower = false;
+        cfg.enable_upper = true;
+        cfg.enable_special = false;
+
+        let alpha = Alphabet::new(&cfg);
+
+        for elem in alpha.chars.iter() {
+            assert_eq!(elem.is_ascii_uppercase(), true);
+        }
+    }
+
+    #[test]
+    fn it_makes_special_only_alphabet() {
+        let mut cfg = ConfigArgs::new();
+        cfg.enable_digit = false;
+        cfg.enable_lower = false;
+        cfg.enable_upper = false;
+        cfg.enable_special = true;
+
+        let alpha = Alphabet::new(&cfg);
+
+        for elem in alpha.chars.iter() {
+            assert_eq!(constants::SPECIAL_CHARS.contains(elem), true);
+        }
+    }
+
+    #[test]
+    fn it_makes_alphabet_with_all() {
+        let mut cfg = ConfigArgs::new();
+        cfg.enable_digit = true;
+        cfg.enable_lower = true;
+        cfg.enable_upper = true;
+        cfg.enable_special = true;
+
+        let alpha = Alphabet::new(&cfg);
+
+        assert_eq!(alpha.chars.len(), MAX_ALPHABET_LENGTH);
+    }
+
+    #[test]
+    fn it_should_produce_known_chars() {
+        let cfg = ConfigArgs::new();
+        let mut alpha = Alphabet::new(&cfg);
+
+        let seed = [0; 32];
+        alpha.set_seed(seed);
+
+        let expected = ['X', '4', 'F', 'a', '1', 'i', 'H', '0', '9', 'R'];
+
+        let mut result = Vec::new();
+
+        for _ in 0..10 {
+            result.push(alpha.get_char());
+        }
+
+        assert_eq!(result, expected);
     }
 }
